7ca6be976ce015b559226844b194b462581d32b8
Alexandre Cardoso <alexmcardoso@gmail.com>
2024-03-22 09:56:06 -0300

    feat: add tarefa controller

--- api/backend.dpr
+++ api/backend.dpr
@@ -4,27 +4,14 @@ program backend;
 
 {$R *.res}
 
-uses Horse, Horse.Jhonson, System.JSON;
+uses
+  Horse,
+  Horse.Jhonson,
+  System.JSON,
+  tarefaController in 'controller\tarefaController.pas';
 
 begin
   THorse.Use(Jhonson());
-  THorse.Get('/ping',
-    procedure(Req: THorseRequest; Res: THorseResponse)
-    begin
-      Res.Send('pong');
-    end);
-
-    THorse.Post('/ping',
-    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
-    var
-      LBody: TJSONObject;
-    begin
-      // Req.Body gives access to the content of the request in string format.
-      // Using jhonson middleware, we can get the content of the request in JSON format.
-
-      LBody := Req.Body<TJSONObject>;
-      Res.Send<TJSONObject>(LBody);
-    end);
-
   THorse.Listen(9000);
+  tarefaController.Router;
 end.
---
+++ api/controller/tarefaController.pas
@@ -0,0 +1,46 @@
+unit tarefaController;
+
+interface
+
+uses
+  Horse,
+  Horse.Jhonson,
+  System.JSON,
+  System.SysUtils;
+
+procedure Router;
+
+implementation
+
+procedure ListarTarefas(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+begin
+end;
+
+procedure AdicionarTarefa(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+begin
+end;
+
+procedure AtualizarTarefa(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+begin
+end;
+
+procedure DeletarTarefa(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+begin
+end;
+
+procedure Router;
+var
+  v1: string;
+begin
+  v1 := '/api/v1';
+  //listar todas as tarefas
+  THorse.Get('/tarefas', ListarTarefas);
+  //adicionar tarefa
+  THorse.Post('/tarefa', AdicionarTarefa);
+  //atualizar status da tarefa
+  THorse.Put('/tarefa', AtualizarTarefa);
+  //remover tarefa
+  THorse.Delete('/tarefa', DeletarTarefa);
+end;
+
+end.
